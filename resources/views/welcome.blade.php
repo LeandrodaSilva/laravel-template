0cf232e227560e882238df2a291da5a05f13e120
LeandroDaSilva <Av@rogue11076>
2021-09-08 03:48:12 -0300

    changed serviceWorker.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -412,7 +412,7 @@
         if ('serviceWorker' in navigator) {
             // Use the window load event to keep the page load performant
             window.addEventListener('load', () => {
-                navigator.serviceWorker.register({{ mix('js/serviceWorker.js') }});
+                navigator.serviceWorker.register('/service-worker.js');
             });
         }
     </script>
