54e1681d5ed679042ddb534a97a61c5bbe88f410
LeandroDaSilva <Av@rogue11076>
2021-09-08 03:53:28 -0300

    changed serviceWorker.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -407,14 +407,9 @@
         }
     </style>
 
-    <script>
-        // Check that service workers are supported
-        if ('serviceWorker' in navigator) {
-            // Use the window load event to keep the page load performant
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/service-worker.js');
-            });
-        }
+    <script type="module">
+        import * as serviceWorker from '/service-worker.js';
+        serviceWorker.register();
     </script>
 </head>
 <body class="antialiased">
