e08b329de290e543035766dd13481680f69bc21c
LeandroDaSilva <Av@rogue11076>
2021-09-07 23:23:22 -0300

    Updated actions

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ln -s /home/leandro/.env .env
-composer install -q --no-ansi --no-interaction --no-scripts --no-progress --prefer-dist
-php artisan migrate --force || php artisan migrate:rollback
-php artisan storage:link
-php artisan optimize
