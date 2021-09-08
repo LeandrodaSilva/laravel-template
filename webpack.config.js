800b2b23a6e7f3c2d9fc23e4533160b8c993685a
LeandroDaSilva <Av@rogue11076>
2021-09-08 05:07:41 -0300

    changed serviceWorker.js

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         } ),
         new GenerateSW({
             modifyURLPrefix: {
-                '/': '/public/'
+                '/': '/'
             },
         }),
     ],
